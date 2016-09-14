35c581beab247b52918ba25bf4d90f755dfccde0
Henrique Leal <hm.leal@hotmail.com>
2016-09-14 14:26:21 -0300

    feature: Implement in lexer method consume.

--- parser.go
+++ parser.go
@@ -9,25 +9,33 @@ func NewParser(l Lexer) Parser {
     return Parser{lexer: l, current_token: l.get_next_token()}
 }
 
-func (p *Parser) term() int {
-    token := p.current_token
-    p.current_token = p.lexer.get_next_token()
-    return token.toInteger()
-}
-
 func (p *Parser) expr() int {
     result := p.term()
 
     for p.current_token.kind == "PLUS" || p.current_token.kind == "MINUS" {
         token := p.current_token
         if token.kind == PLUS {
-            p.current_token = p.lexer.get_next_token()
+            p.consume(PLUS)
             result = result + p.term()
         }
         if token.kind == MINUS {
-            p.current_token = p.lexer.get_next_token()
+            p.consume(MINUS)
             result = result - p.term()
         }
     }
     return result
+}
+
+func (p *Parser) term() int {
+    token := p.current_token
+    p.consume(INTEGER)
+    return token.toInteger()
+}
+
+func (p *Parser) consume(token_type string) {
+    if p.current_token.kind == token_type {
+        p.current_token = p.lexer.get_next_token()
+    } else {
+        panic("Invalid syntax")
+    }
 }
