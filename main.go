9287658d7dda659c5b7ffdf350c70ea603640c34
Henrique Leal <hm.leal@hotmail.com>
2016-09-14 14:08:35 -0300

    feature: Split parser in new file.

--- main.go
+++ main.go
@@ -6,38 +6,6 @@ import (
     "os"
 )
 
-type Parser struct {
-    lexer Lexer
-    current_token Token
-}
-
-func NewParser(l Lexer) Parser {
-    return Parser{lexer: l, current_token: l.get_next_token()}
-}
-
-func (p *Parser) term() int {
-    token := p.current_token
-    p.current_token = p.lexer.get_next_token()
-    return token.toInteger()
-}
-
-func (p *Parser) expr() int {
-    result := p.term()
-
-    for p.current_token.kind == "PLUS" || p.current_token.kind == "MINUS" {
-        token := p.current_token
-        if token.kind == PLUS {
-            p.current_token = p.lexer.get_next_token()
-            result = result + p.term()
-        }
-        if token.kind == MINUS {
-            p.current_token = p.lexer.get_next_token()
-            result = result - p.term()
-        }
-    }
-    return result
-}
-
 type Interpreter struct {
 }
 
