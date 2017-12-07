a3370e9dbe1c8d3bfe51306f2790eba26737a049
Henrique Leal <hm.leal@hotmail.com>
2017-12-07 22:02:28 +0000

    chore: Update docs and move some functions

--- parser.go
+++ parser.go
@@ -10,10 +10,11 @@ func NewParser(l Lexer) Parser {
 	return Parser{lexer: l, currentToken: l.getNextToken()}
 }
 
+// expr Check the expression
+// expr  : term   ((PLUS | MINUS) term)*
+// term  : factor ((MUL | DIV) factor)*
+// factor: INTEGER | (LPAREN expr RPAREN)
 func (p *Parser) expr() int {
-	// expr  : term ((PLUS | MINUS) term)*
-	// term  : factor ((MUL | DIV) factor)*
-	// factor: INTEGER | (LPAREN expr RPAREN)
 	result := p.term()
 
 	for p.currentToken.kind == PLUS || p.currentToken.kind == MINUS {
@@ -30,24 +31,8 @@ func (p *Parser) expr() int {
 	return result
 }
 
-func (p *Parser) factor() int {
-	// factor: INTEGER | (LPAREN expr RPAREN)
-	token := p.currentToken
-	result := 0
-	if token.kind == INTEGER {
-		p.consume(INTEGER)
-		return token.toInteger()
-	} else if token.kind == LPAREN {
-		p.consume(LPAREN)
-		result := p.expr()
-		p.consume(RPAREN)
-		return result
-	}
-	return result
-}
-
+// term : factor ((MUL | DIV) factor)*
 func (p *Parser) term() int {
-	// term : factor ((MUL | DIV) factor)*
 	result := p.factor()
 
 	for p.currentToken.kind == MUL || p.currentToken.kind == DIV {
@@ -60,6 +45,22 @@ func (p *Parser) term() int {
 			p.consume(DIV)
 			result = result / p.factor()
 		}
+	}
+	return result
+}
+
+// factor: INTEGER | (LPAREN expr RPAREN)
+func (p *Parser) factor() int {
+	token := p.currentToken
+	result := 0
+	if token.kind == INTEGER {
+		p.consume(INTEGER)
+		return token.toInteger()
+	} else if token.kind == LPAREN {
+		p.consume(LPAREN)
+		result := p.expr()
+		p.consume(RPAREN)
+		return result
 	}
 	return result
 }
