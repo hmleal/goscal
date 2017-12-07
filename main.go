da814716c14470eb3258855fa6ff4d423aef9642
Henrique Leal <hm.leal@hotmail.com>
2017-12-07 16:53:34 +0000

    feat: It's possible to pass a interface ^^

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// http://golangtutorials.blogspot.ie/2011/06/methods-on-structs.html
 package main
 
 import (
@@ -5,6 +6,21 @@ import (
 	"fmt"
 	"os"
 )
+
+type interpreter struct {
+	parser Parser
+}
+
+// NewInterpreter creates a interpreter
+func (i *interpreter) NewInterpreter() {}
+
+// Interpret visit all nodes in the tree
+func (i *interpreter) Interpret() {
+	///tree := i.parser.expr() // TODO change to .parse
+	//return i.visit()
+}
+
+func (i *interpreter) visit() {}
 
 func main() {
 	fmt.Println("Welcome a implementation of Pascal in GoLang")
