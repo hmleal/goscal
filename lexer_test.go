52a6facae016782e4b6b7d46ad909b7917a65d03
Henrique Leal <hm.leal@hotmail.com>
2017-12-09 17:33:03 +0000

    test: Add some Lexer tests

---
+++ lexer_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+type testpair struct {
+	input  string
+	output Token
+}
+
+var tests = []testpair{
+	{"1", Token{kind: INTEGER, value: "1"}},
+	{"+", Token{kind: PLUS, value: "+"}},
+	{"-", Token{kind: MINUS, value: "-"}},
+	{"*", Token{kind: MUL, value: "*"}},
+	{"/", Token{kind: DIV, value: "/"}},
+	{"(", Token{kind: LPAREN, value: "("}},
+	{")", Token{kind: RPAREN, value: ")"}},
+}
+
+func TestGetNextToken(t *testing.T) {
+	for _, pair := range tests {
+		lexer := NewLexer(pair.input)
+		token := lexer.getNextToken()
+
+		if token.kind != pair.output.kind {
+			t.Error(
+				"For", pair.input,
+				"expected", pair.output.kind,
+				"got", token.kind,
+			)
+		}
+	}
+}
